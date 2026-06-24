1b49556be3c5faca08cf86af3fd2c0a1f5227058
rohithlab123 <vtu20793.soc.ece@gmail.com>
2026-06-25 03:21:23 +0530

    Update ForgotPasswordServlet.java

--- src/main/java/controller/ForgotPasswordServlet.java
+++ src/main/java/controller/ForgotPasswordServlet.java
@@ -1,14 +1,14 @@
 package controller;
 
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 
-import dao.DBConnection;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
-import jakarta.servlet.http.*;
+// CHANGE THESE FROM jakarta TO javax
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 @WebServlet("/ForgotPasswordServlet")
 public class ForgotPasswordServlet extends HttpServlet {
